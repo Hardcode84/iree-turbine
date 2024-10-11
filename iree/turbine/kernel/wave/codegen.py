162f19568a6a7d6bf4f6a3cea0529a5a23bf570f
Ivan Butygin <ivan.butygin@gmail.com>
2024-10-11 17:24:00 +0200

    hack hack
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- iree/turbine/kernel/wave/codegen.py
+++ iree/turbine/kernel/wave/codegen.py
@@ -548,6 +548,9 @@ def _build_mask(
     return mask
 
 
+_skip = True
+
+
 def _construct_gather_scatter_indices(
     emitter: WaveEmitter,
     symbolc_shape: tuple[IndexExpr],
@@ -612,8 +615,11 @@ def _construct_gather_scatter_indices(
         )
         mask = vector_d.constant_mask(mask_vec_type, [elements_per_thread])
 
-    start_indices = _build_start_indices(emitter, result_index)
-    return start_indices, None, mask
+    global _skip
+    if _skip:
+        _skip = False
+        start_indices = _build_start_indices(emitter, result_index)
+        return start_indices, None, mask
 
     start_indices_orig = _get_start_indices(index)
 
