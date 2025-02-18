14167f2abbdabd408b3ec14e6b9970e14c1e1c31
Ivan Butygin <ivan.butygin@gmail.com>
2025-02-18 16:56:25 +0100

    fix
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- iree/turbine/kernel/wave/templates/extend_attention_rpe.py
+++ iree/turbine/kernel/wave/templates/extend_attention_rpe.py
@@ -333,16 +333,13 @@ def get_extend_attention_rpe_kernel(
             # Fused T5 RPE adds attention bias pre-softmax normalization.
             # When fusing into the FA variant, adding locally before the max and
             # the partial softmax should be equivalent.
-            i = tkw.self_index(N_Q, tkl.i64, elements_per_thread=1)
-            i = tkw.broadcast(i, target_shape=[H, N_Q, N_KV])
-            j = tkw.self_index(
-                N_KV, tkl.i64, elements_per_thread=LOAD_ELEMS_PER_THREAD_QK
-            )
+            i = tkw.self_index(N_Q, tkl.i64)
+            j = tkw.self_index(N_KV, tkl.i64)
             rpe_reg = tkw.read(
                 rpe,
                 mapping=rpe_mapping,
                 mapping_dynamic_vals=(i, j),
-                elements_per_thread=LOAD_ELEMS_PER_THREAD_QK,
+                elements_per_thread=STORE_ELEMS_PER_THREAD,
             )
             # Layer and RPE scaling since we use log2 instead of log2
             x_j = x_j * layer_scale_reg + rpe_reg * rpe_scale_reg
