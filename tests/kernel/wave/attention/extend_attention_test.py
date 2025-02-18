f8ef9ad221ce8fe57f6533e2761ab62115539227
Ivan Butygin <ivan.butygin@gmail.com>
2025-02-18 16:47:50 +0100

    asdasd
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- tests/kernel/wave/attention/extend_attention_test.py
+++ tests/kernel/wave/attention/extend_attention_test.py
@@ -4,9 +4,12 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+import math
 import pytest
 import torch
-import math
+from torch.nn import functional as F
+
+
 import iree.turbine.kernel as tk
 from iree.turbine.kernel.lang.global_symbols import *
 from iree.turbine.kernel.wave.utils import (
@@ -21,6 +24,9 @@ from iree.turbine.kernel.wave.constraints import MMAType
 from iree.turbine.kernel.wave.templates.extend_attention import (
     get_extend_attention_kernel,
 )
+from iree.turbine.kernel.wave.templates.extend_attention_rpe import (
+    get_extend_attention_rpe_kernel,
+)
 from iree.turbine.kernel.wave.templates.prefill_attention import (
     get_prefill_attention_kernel,
 )
@@ -28,6 +34,7 @@ from iree.turbine.kernel.wave.templates.attention_common import (
     AttentionShape,
 )
 import os
+from enum import Enum
 from torch.testing import assert_allclose
 from ..common.utils import (
     require_e2e,
@@ -42,6 +49,22 @@ from ..common.shapes import get_test_shapes, construct_test_name
 # Reference paged attention implementation from vLLM and sglang.
 
 
+def t5_rpe_masked_cond(
+    rpe: torch.Tensor, max_rpe_context_length: int, sequence_length: int
+) -> torch.Tensor:
+    positions = torch.arange(sequence_length).to(device=rpe.device)
+    pos_diff = positions.unsqueeze(1) - positions.unsqueeze(0)
+    mask = ((pos_diff >= 0) & (pos_diff < max_rpe_context_length)).to(device=rpe.device)
+    rpe_cond = device_zeros(sequence_length, sequence_length, dtype=rpe.dtype)
+    rpe_cond[mask] = rpe[pos_diff[mask]]
+    return rpe_cond
+
+
+class ScoreMod(Enum):
+    SoftCap = 0
+    RPE = 1
+
+
 def context_attention_fwd(
     q: torch.Tensor,
     k: torch.Tensor,
@@ -49,18 +72,13 @@ def context_attention_fwd(
     o: torch.Tensor,
     b_start_loc: torch.Tensor,
     b_seq_len: torch.Tensor,
-    max_len_in_batch: int,
+    max_len_extend: int,
     is_causal: bool = False,
     logit_cap: float = 0.0,
+    rpe_bias: torch.Tensor = None,
+    score_mod: ScoreMod = ScoreMod.SoftCap,
+    max_rpe_context_length: int = 0,
 ):
-    def soft_cap(score, b, h, q_idx, kv_idx):
-        score = score / logit_cap
-        score = torch.tanh(score)
-        score = score * logit_cap
-        return score
-
-    def causal(b, h, q_idx, kv_idx):
-        return q_idx >= kv_idx
 
     cu_seq_lens = [0] * (len(b_seq_len) + 1)
     for i, seq_len in enumerate(b_seq_len):
@@ -69,24 +87,31 @@ def context_attention_fwd(
     for i in range(len(b_seq_len)):
         start, end = cu_seq_lens[i], cu_seq_lens[i + 1]
         qkv_len = end - start
-        block_mask = (
-            create_block_mask(causal, B=None, H=None, Q_LEN=qkv_len, KV_LEN=qkv_len)
-            if is_causal
-            else None
-        )
-        o_torch = (
-            flex_attention(
-                q[start:end].permute(1, 0, 2).unsqueeze(0),
-                k[start:end].permute(1, 0, 2).unsqueeze(0),
-                v[start:end].permute(1, 0, 2).unsqueeze(0),
-                score_mod=soft_cap,
-                enable_gqa=True,
-                block_mask=block_mask,
+        Q = q[start:end].permute(1, 0, 2)
+        K = k[start:end].permute(1, 0, 2)
+        K = K.expand(Q.shape[0], *K.shape[1:])
+        V = v[start:end].permute(1, 0, 2)
+        V = V.expand(Q.shape[0], *V.shape[1:])
+        dk_sqrt = math.sqrt(1.0 / Q.shape[-1])
+        a = torch.bmm(Q, K.transpose(-1, -2)) * dk_sqrt
+        if ScoreMod == ScoreMod.SoftCap:
+            a = a / logit_cap
+            a = torch.tanh(a)
+            a = a * logit_cap
+        else:
+            rpe_cond = t5_rpe_masked_cond(
+                rpe_bias,
+                max_rpe_context_length=max_rpe_context_length,
+                sequence_length=K.shape[1],
             )
-            .squeeze(0)
-            .permute(1, 0, 2)
-        )
-        o[start:end] = o_torch
+            print(f"\n\nrpe_cond\n{rpe_cond}")
+            print(f"rpe_cond_shape: {rpe_cond.shape}")
+            rpe_cond = rpe_cond.unsqueeze(0)
+            rpe_cond = rpe_cond.expand(Q.shape[0], *rpe_cond.shape[1:])
+            a = a + rpe_cond
+        reference = torch.bmm(F.softmax(a, dim=-1).to(dtype=V.dtype), V)
+        reference = reference.squeeze(0).permute(1, 0, 2)
+        o[start:end] = reference
 
     return o
 
@@ -100,11 +125,14 @@ def ref_extend_attn(
     b_start_loc: torch.Tensor,
     b_seq_len: torch.Tensor,
     b_seq_len_prefix: torch.Tensor,
-    max_len_in_batch: int,
+    max_len_extend: int,
     extend_token_num: int,
     dtype: torch.dtype,
     is_causal: bool = False,
     logit_cap: float = 0.0,
+    rpe_bias: torch.Tensor = None,
+    score_mod: ScoreMod = ScoreMod.SoftCap,
+    max_rpe_context_length: int = 0,
 ) -> torch.Tensor:
     total_token_num = k_buffer.shape[0]
     B, H_Q, D = b_req_idx.shape[0], q_extend.shape[-2], q_extend.shape[-1]
@@ -128,9 +156,12 @@ def ref_extend_attn(
         o_buffer,
         b_start_loc,
         b_seq_len,
-        max_len_in_batch,
+        max_len_extend,
         is_causal,
         logit_cap=logit_cap,
+        rpe_bias=rpe_bias,
+        score_mod=score_mod,
+        max_rpe_context_length=max_rpe_context_length,
     )
 
     pt = 0
@@ -212,6 +243,14 @@ def create_inputs(
     max_len_extend = torch.max(b_seq_len_extend, 0)[0].item()
     logit_cap = 30.0
 
+    max_rpe_context_length = 1
+    rpe_bias = device_zeros(max_rpe_context_length + 1, dtype=torch.float32)
+    rpe_bias.copy_(
+        5 * torch.rand(max_rpe_context_length + 1, dtype=torch.float32, device="cuda")
+    )
+    rpe_bias[max_rpe_context_length] = 0
+    print(f"\n\nrpe_bias\n{rpe_bias}")
+
     return (
         q_extend,
         k_extend,
@@ -229,17 +268,18 @@ def create_inputs(
         extend_token_num,
         max_len_extend,
         logit_cap,
+        rpe_bias,
+        max_rpe_context_length,
     )
 
 
 # TODO: Investigate errors on MI250.
 @require_e2e
-@require_cdna3
+# @require_cdna3
 @pytest.mark.parametrize("shape", get_test_shapes("extend"))
 @pytest.mark.parametrize("dtype", [torch.float16])
 @pytest.mark.parametrize("enable_scheduling", [False])
-@pytest.mark.parametrize("is_causal", [False, True])
-@pytest.mark.parametrize("use_buffer_ops", [False, True])
+@pytest.mark.parametrize("is_causal", [False])
 @pytest.mark.parametrize(
     "mfma_variant",
     [
@@ -252,7 +292,6 @@ def testExtendAttention(
     dtype: torch.dtype,
     enable_scheduling: bool,
     is_causal: bool,
-    use_buffer_ops: bool,
     mfma_variant: MMAType,
     request,
 ):
@@ -276,6 +315,8 @@ def testExtendAttention(
         extend_token_num,
         max_len_extend,
         logit_cap,
+        _,
+        _,
     ) = create_inputs(shape, dtype)
     shape.max_seq_len = max_len_extend
 
@@ -314,8 +355,6 @@ def testExtendAttention(
     if run_bench:
         config["benchmark_batch_size"] = 1000
         config["benchmark_repetitions"] = 3
-        config["dump_intermediates"] = "./inter"
-
     if dump_perf is not None:
         perf_filename = construct_test_name(
             "wave_extend_attention", mfma_variant, is_causal, shape
@@ -332,8 +371,6 @@ def testExtendAttention(
         use_scheduling_barriers=enable_scheduling_barriers,
         dynamic_symbols=dynamic_symbols,
         dynamic_symbols_map=dynamic_symbols_map,
-        use_buffer_load_ops=use_buffer_ops,
-        use_buffer_store_ops=use_buffer_ops,
     ):
         mb_qk = extend_attention(
             q_extend,
@@ -363,7 +400,7 @@ def testExtendAttention(
         b_start_loc=b_start_loc,
         b_seq_len=b_seq_len,
         b_seq_len_prefix=b_seq_len_prefix,
-        max_len_in_batch=max_len_in_batch,
+        max_len_extend=max_len_extend,
         extend_token_num=extend_token_num,
         dtype=dtype,
         is_causal=is_causal,
@@ -371,3 +408,152 @@ def testExtendAttention(
     )
 
     assert_allclose(output, ref_output, rtol=1e-3, atol=1e-3)
+
+
+# TODO: Investigate errors on MI250.
+@require_e2e
+# @require_cdna3
+@pytest.mark.parametrize("shape", get_test_shapes("extend"))
+@pytest.mark.parametrize("dtype", [torch.float16])
+@pytest.mark.parametrize("enable_scheduling", [False])
+@pytest.mark.parametrize("is_causal", [False])
+@pytest.mark.parametrize(
+    "mfma_variant",
+    [
+        (MMAType.F32_16x16x16_F16, MMAType.F32_16x16x16_F16),
+    ],
+)
+def testExtendRpeAttention(
+    shape: list[AttentionShape],
+    dtype: torch.dtype,
+    enable_scheduling: bool,
+    is_causal: bool,
+    mfma_variant: MMAType,
+    request,
+):
+
+    torch.manual_seed(0)
+    assert shape.num_query_heads % shape.num_kv_heads == 0
+    (
+        q_extend,
+        k_extend,
+        v_extend,
+        k_buffer,
+        v_buffer,
+        req_to_tokens,
+        b_req_idx,
+        b_seq_len,
+        b_seq_len_extend,
+        b_start_loc,
+        b_start_loc_extend,
+        b_seq_len_prefix,
+        max_len_in_batch,
+        extend_token_num,
+        max_len_extend,
+        logit_cap,
+        rpe_bias,
+        max_rpe_context_length,
+    ) = create_inputs(shape, dtype)
+    shape.max_seq_len = max_len_extend
+
+    if mfma_variant[0] == MMAType.F32_16x16x16_F16:
+        num_waves = 4
+    if mfma_variant[1] == MMAType.F32_32x32x8_F16:
+        num_waves = 2
+
+    # Run the wave kernel.
+    output = device_zeros(
+        extend_token_num, shape.num_query_heads, shape.head_size, dtype=torch.float32
+    )
+    (
+        extend_attention_rpe,
+        hyperparams,
+        dynamic_symbols,
+        dynamic_symbols_map,
+    ) = get_extend_attention_rpe_kernel(
+        shape,
+        mfma_variant,
+        q_extend.shape,
+        k_extend.shape,
+        v_extend.shape,
+        req_to_tokens.shape,
+        k_buffer.shape,
+        v_buffer.shape,
+        output.shape,
+        is_causal=is_causal,
+        num_waves=num_waves,
+        max_rpe_context_length=max_rpe_context_length,
+    )
+    hyperparams.update(get_default_scheduling_params())
+    config = get_default_run_config()
+    run_bench = request.config.getoption("--runperf")
+    dump_perf = request.config.getoption("--dump-perf-files-path")
+    if run_bench:
+        config["benchmark_batch_size"] = 1000
+        config["benchmark_repetitions"] = 3
+    if dump_perf is not None:
+        perf_filename = construct_test_name(
+            "wave_extend_attention", mfma_variant, is_causal, shape
+        )
+        config["benchmark_results_file"] = os.path.join(dump_perf, perf_filename)
+
+    rpe_debug = torch.zeros((16, 864, 864), dtype=torch.float32, device="cuda")
+    with tk.gen.TestLaunchContext(
+        hyperparams,
+        canonicalize=True,
+        run=True,
+        run_bench=run_bench,
+        # compile_config={"print_ir_after": "all"},
+        run_config=config,
+        schedule=enable_scheduling,
+        use_scheduling_barriers=enable_scheduling_barriers,
+        dynamic_symbols=dynamic_symbols,
+        dynamic_symbols_map=dynamic_symbols_map,
+    ):
+        mb_qk = extend_attention_rpe(
+            q_extend,
+            k_extend,
+            v_extend,
+            k_buffer,
+            v_buffer,
+            req_to_tokens,
+            b_req_idx,
+            b_seq_len,
+            b_seq_len_extend,
+            b_start_loc_extend,
+            rpe_bias,
+            output,
+            rpe_debug,
+        )
+        print(mb_qk.module_op)
+
+    print(f"\n\nrpe_debug\n{rpe_debug[0]}")
+    print(f"rpe_debug_shape: {rpe_debug[0].shape}")
+
+    if dump_generated_mlir:
+        filename = f"wave_extend_attention_kernel_rpe_{'x'.join(map(str, shape))}.mlir"
+        with open(filename, "w") as f:
+            f.write(mb_qk.module_op.get_asm())
+
+    # Run the reference implementation.
+    ref_output = ref_extend_attn(
+        q_extend=q_extend,
+        k_buffer=k_buffer,
+        v_buffer=v_buffer,
+        b_req_idx=b_req_idx,
+        b_start_loc=b_start_loc,
+        b_seq_len=b_seq_len,
+        b_seq_len_prefix=b_seq_len_prefix,
+        max_len_extend=max_len_extend,
+        extend_token_num=extend_token_num,
+        dtype=dtype,
+        is_causal=is_causal,
+        rpe_bias=rpe_bias,
+        logit_cap=logit_cap,
+        score_mod=ScoreMod.RPE,
+        max_rpe_context_length=max_rpe_context_length,
+    )
+
+    torch.testing.assert_close(
+        output, ref_output, rtol=2e-3, atol=2e-3, check_dtype=False
+    )
