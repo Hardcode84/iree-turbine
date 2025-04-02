4d322ecb7f106f5e951466ae8eb000e526803d6c
Ivan Butygin <ivan.butygin@gmail.com>
2025-04-02 20:32:50 +0200

    hach
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- iree/turbine/kernel/ops/wave_ops.py
+++ iree/turbine/kernel/ops/wave_ops.py
@@ -2126,10 +2126,11 @@ class Permute(CustomOp, ABC):
         src_to_target = {
             src: self.target_shape[src_shape.index(src)] for src in src_shape
         }
+
         permuted_index = {
             k: IndexSequence(v.start, v.size, index[src_to_target[k]].stride)
             for k, v in index.items()
-            if k in src_shape
+            if k in src_shape and src_to_target[k] in index
         }
         return permuted_index
 
